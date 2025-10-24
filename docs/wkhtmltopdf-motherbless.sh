9fefdc28cb2bf2294e7dcf4941abdb3b9d30d8e3
lymslive <403708621@qq.com>
2025-10-24 10:25:38 +0800

    docs: bakup generated pdf

---
+++ docs/wkhtmltopdf-motherbless.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wkhtmltopdf toc --toc-header-text "目录" motherbless.html --header-left "[title]" --header-right "https://github.com/lymslive/drama" --footer-center "知识共享 署名-非商业性使用-禁止演绎 4.0 国际 许可协议" --footer-right "[page]" --footer-spacing 2 --header-spacing 2 --footer-font-size 10 --header-font-size 10 --allow . motherbless.pdf
